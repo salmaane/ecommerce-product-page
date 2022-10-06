8628ed877e3bcd4476fb197e5b30286fbe48d54a
Salmane <88793063+salmaane@users.noreply.github.com>
2022-10-06 13:16:24 +0100

    product gallery interactivity completed (JS)

---
+++ javascript.js
@@ -0,0 +1,16 @@
+let imagesBox = document.querySelector(".thumbnails");
+let currentImage = document.querySelector(".current-image");
+
+imagesBox.onclick = function(event) {
+  let target = event.target;
+  if(target.tagName !== 'IMG') return;
+
+  let currentThumbnail = document.querySelector(".current-thumbnail");
+  currentThumbnail.classList.remove("current-thumbnail");
+  let thumbnailWrapper = document.querySelector(".thumbnail-wrapper");
+  thumbnailWrapper.classList.remove("thumbnail-wrapper");
+
+  currentImage.src = target.src;
+  target.classList.add("current-thumbnail");
+  target.parentElement.classList.add("thumbnail-wrapper");
+}
