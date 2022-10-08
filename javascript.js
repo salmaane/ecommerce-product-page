eab47bbe5e1d1cbec851aa69639df20da4dbdc69
Salmane <88793063+salmaane@users.noreply.github.com>
2022-10-08 07:27:49 +0100

    cart system completed (JS)

--- javascript.js
+++ javascript.js
@@ -18,12 +18,49 @@ imagesBox.onclick = function(event) {
 
 let minus = document.getElementById("minus");
 let plus = document.getElementById("plus");
+let count = document.getElementById("count");
 
 minus.onclick = function(){
-  let count = document.getElementById("count");
   if(count.innerHTML === '0') return;
   count.innerHTML = `${parseInt(count.innerHTML) - 1}`;
 }
 plus.onclick = function(){
   count.innerHTML = `${parseInt(count.innerHTML) + 1}`;
+}
+
+
+let cart = document.querySelector('.cart');
+cart.onclick = function() {
+  let cartContent = document.querySelector(".cart-content");
+  if(cartContent.style.display === '') {
+    cartContent.style.display = 'block';
+  }else {
+    cartContent.style.display = '';
+  }
+}
+
+
+let addToCart = document.querySelector(".add");
+let content = document.querySelector(".content");
+addToCart.onclick = function() {
+  let productsNumber = document.getElementById('count').innerHTML;
+  if(productsNumber === '0') return;
+
+  let count = document.querySelector(".count");
+  let total = document.querySelector(".total");
+  let icon = document.getElementById('count-icon');
+
+  count.innerHTML = productsNumber;
+  total.innerHTML = `$${productsNumber*125}.00`;
+  icon.innerHTML = productsNumber;
+  icon.style.display = 'inline';
+  content.style.display = 'flex';
+}
+
+
+let deleteItem = document.querySelector('.delete');
+  deleteItem.onclick = function() {
+    let icon = document.getElementById('count-icon');
+    icon.style.display = 'none';
+    content.style.display = '';
 }
